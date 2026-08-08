faad3150ff5a4afac0f0387d7bd99840175e46a6
Getachew Zemicheal <michaeltadessemiki@gmail.com>
2026-08-08 11:29:07 +0300

    Update admin password to secure value

--- docker_start.sh
+++ docker_start.sh
@@ -48,10 +48,13 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'michaeltadessemiki@gmail.com', 'admin123')
+    User.objects.create_superuser('admin', 'michaeltadessemiki@gmail.com', 'Michael@2026#Secure!')
     print('Superuser created.')
 else:
-    print('Superuser already exists.')
+    user = User.objects.get(username='admin')
+    user.set_password('Michael@2026#Secure!')
+    user.save()
+    print('Superuser password updated.')
 "
 
 echo "Starting server..."
